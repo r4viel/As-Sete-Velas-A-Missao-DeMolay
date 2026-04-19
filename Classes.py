f2f1a09b6c2e001e4ffd5f03ead9bb4d61019301
raviel <127047447+r4viel@users.noreply.github.com>
2026-04-19 13:40:51 -0300

    Add files via upload

---
+++ Classes.py
@@ -0,0 +1,7 @@
+# classeDosPersonagens
+class Personagem:
+    def __init__(self, nome, vida, forca, poder):
+        self.nome = nome
+        self.vida = vida
+        self.forca = forca
+        self.poder = poder
